91374dc36f3e0531d3687846c342bb08ea0a7b51
nika19du <qnkovanikol29@gmail.com>
2023-03-12 13:37:03 +0200

    strings exercise

--- exercise/strings-demo/src/Main.java
+++ exercise/strings-demo/src/Main.java
@@ -2,15 +2,27 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
+        /*Scanner scanner = new Scanner(System.in);
         System.out.print("Enter a string: ");
         String input = scanner.nextLine();
-
         //Extract the first three characters of the string
-        String substing1= input.substring(0,3);
-        System.out.println("Substring 1: " + substing1);
+        String substring1= input.substring(0,3);
+        System.out.println("Substring 1: " + substring1);
+        scanner.close();*/
 
-        scanner.close();
+        //Common Elements
+        Scanner scanner = new Scanner(System.in);
+
+        String[] words1 = scanner.nextLine().split(" ");
+        String[] words2 = scanner.nextLine().split(" ");
+
+        for(String word2 : words2){
+            for (String word1 : words1){
+                if (word2.equalsIgnoreCase(word1)){
+                    System.out.println(word2 + " ");
+                    break;
+                }
+            }
+        }
     }
 }
